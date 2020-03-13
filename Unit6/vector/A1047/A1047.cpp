062fe4dcbce24dac9eaf4af66fad900f2fb11b30
kagaya <kagaya85@outlook.com>
2020-03-13 15:28:55 +0800

    finish A1047

--- Unit6/vector/A1047/A1047.cpp
+++ Unit6/vector/A1047/A1047.cpp
@@ -6,7 +6,35 @@
 
 using namespace std;
 
+const int maxN = 40010;
+const int maxK = 2510;
+char name[maxN][5];
+vector<int> course[maxK];
+
+bool cmp(int a, int b) {
+	return strcmp(name[a], name[b]) < 0;
+}
+
 int main() {
+	int N, K;
+
+	scanf("%d%d", &N, &K);
+	
+	for(int i = 0; i < N; i++){
+		int n, courseId;
+		scanf("%s%d", name[i], &n);
+		for(int j = 0; j < n; j++) {
+			scanf("%d", &courseId);
+			course[courseId].push_back(i);
+		}
+	}
+
+	for(int i = 1; i <= K; i++){
+		printf("%d %d\n", i, course[i].size());
+		sort(course[i].begin(), course[i].end(), cmp);
+		for(int j = 0; j < course[i].size(); j++)
+			printf("%s\n", name[course[i][j]]);
+	}
 
 	return 0;
 }
