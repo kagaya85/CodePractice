e553909f675afc9cc2d4a5f4412f4d401e4e524b
Kagaya <kagaya85@outlook.com>
2020-10-05 14:44:36 +0800

    finish 18

---
+++ leetCode/18四数之和/18.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"sort"
+)
+
+func main() {
+
+}
+
+func fourSum(nums []int, target int) (ans [][]int) {
+	n := len(nums)
+	sort.Ints(nums)
+
+	for i := 0; i < n-3 && nums[i]+nums[i+1]+nums[i+2]+nums[i+3] <= target; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < n-2 && nums[i]+nums[j]+nums[j+1]+nums[j+2] <= target; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			for left, right := j+1, n-1; left < right; {
+				if sum := nums[i] + nums[j] + nums[left] + nums[right]; sum == target {
+					ans = append(ans, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left++; left < right && nums[left] == nums[left-1]; left++ {
+					}
+					for right--; left < right && nums[right] == nums[right+1]; right-- {
+					}
+				} else if sum < target {
+					left++
+				} else {
+					right--
+				}
+
+			}
+		}
+	}
+	return
+}
