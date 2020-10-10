bb95960e31d75dd90bab3f133b3b4f0443308e24
Kagaya <kagaya85@outlook.com>
2020-10-10 10:37:59 +0800

    finish 142 快慢指针

---
+++ leetCode/142环形链表2/142.go
@@ -0,0 +1,32 @@
+package main
+
+// ListNode 链表
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if slow == fast {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
