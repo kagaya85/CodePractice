6d54ff78780113df601b51b246e200680de576e9
Kagaya <kagaya85@outlook.com>
2020-10-17 13:04:18 +0800

    finish 52

---
+++ leetCode/52N皇后2/52.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+}
+
+func totalNQueens(n int) (ans int) {
+
+	var dfs func(int, int, int, int)
+	dfs = func(row, col, leftd, rightd int) {
+		if row >= n {
+			ans++
+			return
+		}
+
+		bits := ^(col | leftd | rightd) & ((1 << n) - 1)
+		for bits > 0 {
+			pick := bits & -bits // 与自己的补码做与操作，获取最后一位1
+			dfs(row+1, col|pick, (leftd|pick)<<1, (rightd|pick)>>1)
+			bits &= bits - 1 // 与自己-1做与操作，将最后一位1清零
+		}
+	}
+
+	dfs(0, 0, 0, 0)
+
+	return
+}
