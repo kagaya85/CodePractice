17d96963e5497e93282a4be7d4a07c3582daafeb
Kagaya <kagaya85@outlook.com>
2020-10-17 13:06:58 +0800

    add 完善52注释

--- leetCode/52N皇后2/52.go
+++ leetCode/52N皇后2/52.go
@@ -13,9 +13,10 @@ func totalNQueens(n int) (ans int) {
 			return
 		}
 
+		// 将01反转，并清空n位以前的1，反转后1所在的位置可以摆放皇后
 		bits := ^(col | leftd | rightd) & ((1 << n) - 1)
 		for bits > 0 {
-			pick := bits & -bits // 与自己的补码做与操作，获取最后一位1
+			pick := bits & -bits // 与自己的补码做与操作，获取最后一位1（可放皇后的位置）
 			dfs(row+1, col|pick, (leftd|pick)<<1, (rightd|pick)>>1)
 			bits &= bits - 1 // 与自己-1做与操作，将最后一位1清零
 		}
