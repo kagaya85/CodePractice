2ed2864698a96da2ed506bfd0f0079d3d57fbef2
Kagaya <kagaya85@outlook.com>
2020-10-18 14:19:46 +0800

    finish 19

---
+++ leetCode/19删除链表的倒数第N个节点/19.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	headNode := &ListNode{0, head}
+	p, q := headNode, head
+
+	for i := 0; i < n; i++ {
+		q = q.Next
+	}
+
+	for q != nil {
+		p = p.Next
+		q = q.Next
+	}
+
+	// delete p next
+	p.Next = p.Next.Next
+
+	return headNode.Next
+}
