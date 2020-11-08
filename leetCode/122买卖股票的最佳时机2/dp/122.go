070b80bdf98386fb5e27be4ce94cbbc916cdf9bf
Kagaya <kagaya85@outlook.com>
2020-11-08 10:36:21 +0800

    finish 122 dp

---
+++ leetCode/122买卖股票的最佳时机2/dp/122.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+func maxProfit(prices []int) (ans int) {
+	n := len(prices)
+	dp := make([][2]int, n)
+	dp[0][1] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])
+	}
+	return dp[n-1][0]
+}
+
+func max(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
