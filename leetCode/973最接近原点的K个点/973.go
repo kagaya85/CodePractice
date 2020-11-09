5d30227b25a2712f34b724c203d2c445cd141b0b
Kagaya <kagaya85@outlook.com>
2020-11-09 10:40:31 +0800

    finish 973

---
+++ leetCode/973最接近原点的K个点/973.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+
+func kClosest(points [][]int, K int) [][]int {
+	sort.Slice(points, func(i, j int) bool {
+		p, q := points[i], points[j]
+		return p[0]*p[0]+p[1]*p[1] < q[0]*q[0]+q[1]*q[1]
+	})
+	return points[:K]
+}
