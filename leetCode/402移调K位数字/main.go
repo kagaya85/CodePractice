bba49591517ed6a17792833e00f73c3e20e9f58c
Kagaya <kagaya85@outlook.com>
2020-11-15 22:26:05 +0800

    finish 402

---
+++ leetCode/402移调K位数字/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "strings"
+
+func main() {
+
+}
+
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if len(ans) == 0 {
+		ans = "0"
+	}
+	return ans
+}
