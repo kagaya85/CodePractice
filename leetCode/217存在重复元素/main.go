ee2c7b7e073d780ea7bd4444e08c5f98c51a7494
Kagaya <kagaya85@outlook.com>
2020-12-13 12:19:49 +0800

    finish 217

---
+++ leetCode/217存在重复元素/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+func containsDuplicate(nums []int) bool {
+	set := map[int]struct{}{}
+	for _, v := range nums {
+		if _, has := set[v]; has {
+			return true
+		}
+		set[v] = struct{}{}
+	}
+	return false
+}
