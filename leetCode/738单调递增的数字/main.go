ef07ea643f3051ee9595ef12f17d9eed467280be
Kagaya <kagaya85@outlook.com>
2020-12-15 10:31:44 +0800

    finish 738

---
+++ leetCode/738单调递增的数字/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "strconv"
+
+func main() {
+
+}
+
+func monotoneIncreasingDigits(N int) int {
+	s := []byte(strconv.Itoa(N))
+	i := 1
+	for ; i < len(s) && s[i] >= s[i-1]; i++ {
+	}
+	if i < len(s) {
+		for i > 0 && s[i] < s[i-1] {
+			s[i-1]--
+			i--
+		}
+		for i++; i < len(s); i++ {
+			s[i] = '9'
+		}
+	}
+	ans, _ := strconv.Atoi(string(s))
+	return ans
+}
