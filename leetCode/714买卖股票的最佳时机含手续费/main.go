9da4e50e19561e632b07727416a3760c19d4174c
Kagaya <kagaya85@outlook.com>
2020-12-17 21:45:20 +0800

    finish 714

---
+++ leetCode/714买卖股票的最佳时机含手续费/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+func maxProfit(prices []int, fee int) int {
+	n := len(prices)
+	buy := prices[0] + fee
+	profit := 0
+	for i := 1; i < n; i++ {
+		if prices[i]+fee < buy {
+			buy = prices[i] + fee
+		} else if prices[i] > buy {
+			profit += prices[i] - buy
+			buy = prices[i]
+		}
+	}
+	return profit
+}
