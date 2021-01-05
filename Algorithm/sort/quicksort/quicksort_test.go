63f031d867ed537e29042d32fdceea342702fa67
Kagaya <kagaya85@outlook.com>
2021-01-05 20:46:03 +0800

    test 增加测试用例

--- Algorithm/sort/quicksort/quicksort_test.go
+++ Algorithm/sort/quicksort/quicksort_test.go
@@ -14,4 +14,5 @@ func TestQuickSort(t *testing.T) {
 
 	assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, QuickSort([]int{5, 6, 1, 4, 2, 3}))
 
+	assert.Equal(t, []int{0, 0, 1, 1, 2, 5}, QuickSort([]int{5, 1, 1, 2, 0, 0}))
 }
