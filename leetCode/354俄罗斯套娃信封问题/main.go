f52e6011da8c5b50962487bb827cf8fa45d68cbb
Kagaya <kagaya85@outlook.com>
2021-03-04 21:14:18 +0800

    finish 354

---
+++ leetCode/354俄罗斯套娃信封问题/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "sort"
+
+func maxEnvelopes(envelopes [][]int) int {
+	sort.Slice(envelopes, func(i, j int) bool {
+		a, b := envelopes[i], envelopes[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+
+	f := []int{}
+	for _, e := range envelopes {
+		h := e[1]
+		if i := sort.SearchInts(f, h); i < len(f) {
+			f[i] = h
+		} else {
+			f = append(f, h)
+		}
+	}
+	return len(f)
+}
