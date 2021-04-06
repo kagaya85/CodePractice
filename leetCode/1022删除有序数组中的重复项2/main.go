fb7f9b3e27c4e4b03b4e4162585cae0ef6856e4e
Kagaya <kagaya85@outlook.com>
2021-04-06 21:16:52 +0800

    finish 1022

---
+++ leetCode/1022删除有序数组中的重复项2/main.go
@@ -0,0 +1,25 @@
+package main
+
+// 给你一个有序数组 nums ，
+// 请你 原地 删除重复出现的元素，
+// 使每个元素 最多出现两次 ，
+// 返回删除后数组的新长度。
+
+// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+func removeDuplicates(nums []int) int {
+	n := len(nums)
+	if n <= 2 {
+		return n
+	}
+
+	slow, fast := 2, 2
+	for fast < n {
+		if nums[slow-2] != nums[fast] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
