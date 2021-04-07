9471f21fa4027ef7da4a50457a0e7ec69a9abd11
Kagaya <kagaya85@outlook.com>
2021-04-07 22:46:11 +0800

    finish 81

---
+++ leetCode/81搜索旋转排序数组2/main.go
@@ -0,0 +1,39 @@
+package main
+
+func search(nums []int, target int) bool {
+	n := len(nums)
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		return nums[0] == target
+	}
+
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[l] == nums[mid] && nums[mid] == nums[r] {
+			l++
+			r--
+		} else {
+			// 只搜索有序的部分
+			if nums[l] <= nums[mid] {
+				if nums[l] <= target && target < nums[mid] {
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			} else {
+				if nums[mid] < target && target <= nums[r] {
+					l = mid + 1
+				} else {
+					r = mid - 1
+				}
+			}
+		}
+	}
+	return false
+}
