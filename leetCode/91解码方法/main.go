23281e4bd4378c4224868a51118a8adaf3d60ce4
Kagaya <kagaya85@outlook.com>
2021-04-21 23:28:37 +0800

    finish 91

---
+++ leetCode/91解码方法/main.go
@@ -0,0 +1,20 @@
+package main
+
+func numDecodings(s string) int {
+	n := len(s)
+	dp := make([]int, n+1)
+	dp[0] = 1
+
+	for i := 1; i <= n; i++ {
+		// 转换一个数字
+		if s[i-1] != '0' {
+			dp[i] = dp[i-1]
+		}
+		// 转换两个数字
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0') <= 26) {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[n]
+}
