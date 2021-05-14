b4e5c15046badbd6b1f3c30c5c69014f465ac001
Kagaya <kagaya85@outlook.com>
2021-05-15 00:04:13 +0800

    finish 12

---
+++ leetCode/12整数转罗马数字/main.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+	thousands = [...]string{"", "M", "MM", "MMM"}
+	hundreds  = [...]string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	tens      = [...]string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	ones      = [...]string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+)
+
+func intToRoman(num int) string {
+	return thousands[num/1000] + hundreds[num%1000/100] + tens[num%100/10] + ones[num%10]
+}
