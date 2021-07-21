7bc8fbeab6bb2754ae5a4169dc42b1c0230e2508
Kagaya <kagaya85@outlook.com>
2021-07-21 21:40:01 +0800

    finish 52

---
+++ SwordToOffer/52两个链表的第一个公共节点/main.go
@@ -0,0 +1,27 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	p, q := headA, headB
+
+	for p != q {
+		if p == nil {
+			p = headB
+		} else {
+			p = p.Next
+		}
+		if q == nil {
+			q = headA
+		} else {
+			q = q.Next
+		}
+	}
+	return p
+}
