162f8a1d25c4f97ab3468532ed65ea222474a2a1
Kagaya <kagaya85@outlook.com>
2021-07-25 22:11:50 +0800

    finish 1743

---
+++ leetCode/1743从相邻元素对还原数组/main.go
@@ -0,0 +1,30 @@
+package main
+
+func restoreArray(adjacentPairs [][]int) []int {
+	n := len(adjacentPairs) + 1
+	g := make(map[int][]int, n)
+	for _, p := range adjacentPairs {
+		v, w := p[0], p[1]
+		g[v] = append(g[v], w)
+		g[w] = append(g[w], v)
+	}
+
+	ans := make([]int, n)
+	for e, adj := range g {
+		if len(adj) == 1 {
+			ans[0] = e
+			break
+		}
+	}
+
+	ans[1] = g[ans[0]][0]
+	for i := 2; i < n; i++ {
+		adj := g[ans[i-1]]
+		if ans[i-2] == adj[0] {
+			ans[i] = adj[1]
+		} else {
+			ans[i] = adj[0]
+		}
+	}
+	return ans
+}
