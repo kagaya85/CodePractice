5331a1d1155f11ffbfab7a3f268db820bc009dbe
Kagaya <kagaya85@outlook.com>
2022-10-15 22:10:40 +0800

    finish 1038

---
+++ leetCode/1038更大和树/main.go
@@ -0,0 +1,16 @@
+package main
+
+func bstToGst(root *TreeNode) *TreeNode {
+	sum := 0
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node != nil {
+			dfs(node.Right)
+			sum += node.Val
+			node.Val = sum
+			dfs(node.Left)
+		}
+	}
+	dfs(root)
+	return root
+}
